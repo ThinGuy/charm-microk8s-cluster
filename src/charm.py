ab441ad07e2376a90b7dda9a2788ab3689597f2f
ubuntu <ubuntu@orangebox20.maas.orangebox.me>
2020-10-11 22:01:52 -0700

    working on ops changes from doc

--- src/charm.py
+++ src/charm.py
@@ -8,100 +8,74 @@ import os
 import subprocess
 import random
 import string
-import socket
 import yaml
 from pathlib import Path
-import apt
-
-from ops.charm import CharmBase
-from ops.main import main
+from ops.charm import CharmBase, CharmEvents
 from ops.framework import StoredState
-from ops.model import ActiveStatus, BlockedStatus, MaintenanceStatus, WaitingStatus, Network
+from ops.main import main
+from ops.model import (
+    ActiveStatus,
+    BlockedStatus,
+    MaintenanceStatus,
+    WaitingStatus,
+	Network,
+)
 
 logger = logging.getLogger(__name__)
 
 class Microk8sClusterCharm(CharmBase):
 	"""MicroK8s Cluster Charm."""
-	stored = StoredState()
-
+	_stored = StoredState()
+	
 	def __init__(self, *args):
-		"""Charm initialization for events observation."""
 		super().__init__(*args)
-		self.framework.observe(self.on.install, self)
-		self.framework.observe(self.on.configure, self)
-		if not self.unit.is_leader():
-			 self.unit.status = WaitingStatus("Waiting for leadership")
-			 return
+		self.framework.observe(self.on.install, self._on_install)
+		self.framework.observe(self.on.start, self._on_start)
+		self.framework.observe(self.on.remove, self._on_remove)
+		# -- initialize states --
+		self._stored.set_default(installed=False)
+		self._stored.set_default(started=False)
+		self._stored.set_default(clustered=False)
+		self._stored.set_default(remove=False)
+		self._stored.set_default(unitname=os.environ["JUJU_UNIT_NAME"])
 
-	def on_install(self, event):
-		"""Handle install state."""
+	def _on_install(self, event):
+		if self.state.installed:
+			return
 		self.unit.status = MaintenanceStatus("Installing microk8s")
-		self.state.installed = snap_install('microk8s')
+		cmd = 'sudo snap install microk8s --classic'
+		subprocess.call(cmd, shell=True)
+		self._stored.installed = True
 
-	def check_tokens():
-		if not self.unit.is_leader():
-			return
-		cmd = 'awk 2>/dev/null -F"|" '"'"'/\||^$/{if ($1=="") next;T=$1;NOW=(strftime(systime()));EXP=(strftime("%H:%M:%S",($2-NOW)));if ($2 -gt NOW) print T;next}'"'"' /var/snap/microk8s/current/credentials/cluster-tokens.txt'
-		active_token = "'cluster_token': " + subprocess.check_output(cmd).decode('utf-8').strip() + "'"
-		if isspace(active_token) == False:
-			self.stored.cluster_token = active_token
-		else:
-			set_cluster_token()
-
-	def create_token():
-		if not self.unit.is_leader():
-			return
-		ttl = charm_config.get('token-ttl')
-		length = (32 - (len(str(ttl))))
-		x = ''.join(random.choice(string.ascii_uppercase + string.ascii_lowercase + string.digits) for _ in range(length))
-		new_token = ''.join(x + str(ttl))
-		if isspace(active_token) == False:
-			self.stored.cluster_token = new_token()
-
-	def init_cluster():
-		if not self.unit.is_leader():
-			return
+	def _on_start(self, event):
 		if not self.state.installed:
 			event.defer()
-		if not self.state.configured:
-			event.defer()
-		cluster_token = create_token().strip()
-		ttl = charm_config.get('token-ttl')
-		join_str = 'sudo microk8s.add-node --token '+ cluster_token +' --token-ttl '+ ttl +'|awk '"'"'/^microk8s join/{printf $NF}'"'"''
-		self.stored.leader_clustered = True
-		self.stored.join_cmd = join_str
-
-	def join_cluster():
-		if self.unit.is_leader():
-			return
-		if not self.state.installed:
-			event.defer()
-		if not self.state.configured:
-			event.defer()
-		if not self.state.join_cmd:
-			event.defer()
-		cmd = self.state.join_cmd
-		self.stored.leader_clustered = True
-		self.stored.join_string = join_str
-
-	def on_configure(self, event):
-		if not self.state.installed:
-			event.defer()
+		if self.state.started:
+			return		
+		self.unit.status = MaintenanceStatus("Configuring microk8s")
 		cmd1 = 'sudo usermod -a -G microk8s $(id -un 1000)'
 		cmd2 = 'sudo chown -f -R $(id -un 1000) /home/$(id -un 1000)/.kube'
 		subprocess.call(cmd1, shell=True)
 		subprocess.call(cmd2, shell=True)
-		self.stored.configured = True
+		self._stored.started = True
+		init_cluster()
+
+	def init_cluster():
+		if not self.state.started:
+			event.defer()
 		if not self.unit.is_leader():
-			self.unit.status = WaitingStatus("Waiting for cluster join string")
+			self.unit.status = WaitingStatus("Waiting for leadership")
 			return
+		token = ''.join(random.choice(string.ascii_uppercase + string.ascii_lowercase + string.digits) for _ in range(length))
+		cmd = 'sudo microk8s.add-node --token '+ token +' --token-ttl=900'
+		subprocess.call(cmd, shell=True)
+		self._stored.cluster_token = token
 
 	def _on_remove(self, event):
-		"""Remove Node from Cluster"""
 		self.unit.status = MaintenanceStatus("Leaving cluster")
-		self.stored.started = False
 		cmd = 'microk8s leave'
 		subprocess.call(cmd, shell=True)
+		self._stored.remove = True
 
 if __name__ == "__main__":
 	main(Microk8sClusterCharm)
